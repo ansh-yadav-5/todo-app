dd97ac098c7513a912dd33e210bd634c39806e1a
Ansh Yadav <ansh@example.com>
2026-02-15 00:35:40 +0530

    UI Complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,47 @@
-
 function App() {
-  
+  return (
+    <center class="todo-container">
+      <h1>TODO APP</h1>
+      <div class="container text-center">
+        <div class="row">
+          <div class="col-6">
+            <input type="text" placeholder="Enter TODO Here" />
+            </div>
+          <div class="col-4">
+            <input type="date" />
+          </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-success">Add</button>
+          </div>
+        </div>
 
-  return <div>placeholder</div>
-    
-  
+        <div class="row">
+          <div class="col-6">
+            Buy Milk 
+            </div>
+          <div class="col-4">
+            14/2/2026
+          </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-danger">Delete</button>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-6">
+            Go to College
+            </div>
+          <div class="col-4">
+            14/2/2026
+          </div>
+          <div class="col-2">
+            <button type="button" class="btn btn-danger">Delete</button>
+          </div>
+        </div>
+
+      </div>
+    </center>
+  );
 }
 
-export default App
+export default App;
