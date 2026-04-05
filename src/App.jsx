c929fff6a0aba8b36bf49083ef9be2d7ccf4aaef
Ansh Yadav <ansh@example.com>
2026-04-05 23:48:13 +0530

    Error Resolved & Icons Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
 import AddTodo from "./components/AddTodo";
 import AppName from "./components/AppName";
-import "./App.css";
-
 import TodoItems from "./components/TodoItems";
 import { useState } from "react";
 import WelcomeMessage from "./components/WelcomeMessage";
+import "./App.css"; 
 
 function App() {
-  
-
-  const [todoItmes, setTodoItems] = useState([]);
+  const [todoItems, setTodoItems] = useState([]);
 
   const handleNewItems = (itemName, itemDueDate) => {
-    console.log(`New item Added: ${itemName} Date: ${itemDueDate}`);
-    const newTodoItems = [...todoItmes, { name: itemName, dueDate: itemDueDate }];
+    const newTodoItems = [
+      ...todoItems,
+      { name: itemName, dueDate: itemDueDate },
+    ];
     setTodoItems(newTodoItems);
   };
 
   const handleDeleteItem = (todoItemName) => {
-      const newTodoItems = todoItmes.filter((item) => item.name !== todoItemName);
-      setTodoItems(newTodoItems);
-
-   
-  }
+    const newTodoItems = todoItems.filter(
+      (item) => item.name !== todoItemName
+    );
+    setTodoItems(newTodoItems);
+  };
 
   return (
-    <div className="todo-container">
+    <div className="todoContainer">
       <AppName />
       <AddTodo onNewItem={handleNewItems} />
-      {todoItmes.length === 0 && <WelcomeMessage todoItems={todoItmes}></WelcomeMessage>}
-      <TodoItems todoItems={todoItmes} onDeleteClick={handleDeleteItem}></TodoItems>
+
+      {todoItems.length === 0 && (
+        <WelcomeMessage />
+      )}
+
+      <TodoItems
+        todoItems={todoItems}
+        onDeleteClick={handleDeleteItem}
+      />
     </div>
   );
 }
